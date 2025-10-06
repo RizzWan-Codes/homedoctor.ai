3134baee81406b753c0b2be004be9669dec3edc7
RizzWan-Codes <rizwanboss84@gmail.com>
2025-10-06 17:40:39 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// service-worker.js
+const CACHE_NAME = "homedoctor-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/login.html",
+  "/dashboard.html",
+  "/hello.png",
+  "/manifest.json",
+  "/service-worker.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
